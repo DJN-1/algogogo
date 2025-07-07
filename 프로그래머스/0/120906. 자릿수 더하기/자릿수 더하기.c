a4bf7b2eff95fdc478590e075b5593dd588672f6
Yvette <69054921+DJN-1@users.noreply.github.com>
2025-07-07 19:28:55 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    
+    while (1)
+    {
+        answer = answer + (n % 10);
+        n = n / 10;
+        if(n == 0)
+            break;
+    }
+    
+    return answer;
+}
